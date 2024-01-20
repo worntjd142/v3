a285640b1972c0b1995a35c86500e019246f76b9
wornt <wornt@211.219.243.36>
2024-01-20 21:07:07 +0900

    수주서 함 삭제 수주내역과 병합

---
+++ bin/src/main/webapp/WEB-INF/views/p_manage.jsp
@@ -0,0 +1,101 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<link rel = "stylesheet" href = "resources/css/p_manage.css">
+<link rel = "stylesheet" href = "resources/css/p_detail.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script type="text/javascript" src = "resources/js/p_manage.js"></script>
+<meta charset="UTF-8">
+<title>제품 관리</title>
+</head>
+<body>
+ <!--전체 div-->
+<div class = main_container>
+<!--등록창 div -->
+	<div class = col-md-8>
+		<div class = "comment"><h4>신규 제품 등록</h4></div>
+		<form action = "p_insert" method = "post">
+		<input type = "hidden" name = "pcode" id = "randomcode">
+		<div>
+		<table id = "p_insertt">
+		<tr>
+			<th>제품명</th>
+			<td><input type = "text" name = "pname"></td>
+			<th>제품단가</th>
+			<td><input type = "text" name = "pprice"></td>
+		</tr>
+		<tr>
+			<th>제조사</th>
+			<td><input type = "text" name = "pmc"></td>
+			<th>재고 수</th>
+			<td><input type = "text" name = "pstock"></td>
+		</tr>
+		<tr>
+			<th>제품설명</th>
+			<td><input type = "text" name = "pmi"></td>
+		</tr>
+		</table>
+		</div>
+		<input type = "submit">
+		</form>
+	</div>
+<!-- 제품 리스트 div -->
+	<div class = "col-md-8">
+		<div class = "comment"><h4>제품목록</h4></div>
+		<table id = "p_list">
+			<thead>
+				<tr>
+					<th>제품코드</th>
+					<th>제품명</th>
+					<th>제품단가</th>
+					<th>재고 수</th>
+				</tr>
+			</thead>
+			<tbody>
+		<c:forEach items = "${p_list}" var = "pro" varStatus="a">
+				<tr class = "datacount">
+					<td class="ss${a.index}">${pro.pcode}</td>
+					<td class="ss${a.index}">${pro.pname}</td>
+					<td class="ss${a.index}">${pro.pprice}</td>
+					<td class="ss${a.index}">${pro.pstock}</td>
+				</tr>
+		</c:forEach>
+			</tbody>
+		</table>
+		
+	</div>
+<!-- 상세내역 div -->
+	<div class = "col-md-8">
+		<div class = "comment"><h4>제품 상세 정보</h4></div>
+		<table id = "p_detail" >
+			<tr>
+				<th>제품 코드</th>
+				<td>$제품 코드</td>
+			</tr>
+			<tr>
+				<th>제품명</th>
+				<td>$제품명</td>
+				<th>제품단가</th>
+				<td>$제품단가</td>
+			</tr>
+			<tr>
+				<th>제조사</th>
+				<td>$제품단가</td>
+				<th>재고 수</th>
+				<td>$재고 수</td>
+			</tr>
+			<tr>
+				<th>제품설명</th>
+				<td>$제품설명</td>
+			</tr>
+		</table>
+	</div>
+
+</div>
+</body>
+</html>
--- src/main/java/we/are/Controller/OrderController.java
+++ src/main/java/we/are/Controller/OrderController.java
@@ -1,5 +1,7 @@
 package we.are.Controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import we.are.Model.BaljuDTO;
 import we.are.Model.InventoryDTO;
 import we.are.Service.BaljuService;
+import we.are.Service.SujuService;
+
 
 @Controller
 public class OrderController {
@@ -20,17 +25,39 @@ public class OrderController {
 	@Autowired
 	BaljuService bals;
 	
+	@Autowired
+	SujuService ss;
+	
 	//임시등록 수주서
 	@GetMapping("sujuletter")
-	public String sujuletter() {
-		return "sujuletter";
+	public void sujuletter(BaljuDTO bt, HttpSession session,
+			@RequestParam("day") String day, @RequestParam("number") String bno, @RequestParam("name") String name, Model model,
+			@RequestParam("count") int count, @RequestParam("total") int total, @RequestParam("product") String product, @RequestParam("price") int price,
+			@RequestParam("number") String number) {
+		// url 주수로 받아온 값을 DTO에 하나씩 저장
+		bt.setBaljuday(day); // 발주 일자 or 수주 일자
+		bt.setBno(bno); // 수주번호
+		bt.setBname(name); // 거래처 
+		bt.setBcount(count);// 물품 갯수
+		bt.setBsum(total);// 합계
+		bt.setBproduct(product);// 제품 이름
+		bt.setPprice(price);// 단가
+		bt.setBnumber(number);//사업자 번호
+		
+		// <c:forEach> 를 쓰기위해선 배열형태로 변경해야 하므로 
+		ArrayList<BaljuDTO> bt1 = new  ArrayList<>(); //배열 선언 
+		
+			bt1.add(bt); // bt1의 배열 인덱스 0번에 BaljuDTO타입 bt값을 넣음.
+			
+		model.addAttribute("sujuletter", bt1);	//배열 bt1에 저장된 값을 model 객체의 "sujuletter"라는 변수로 저장.
+		session.setAttribute("maillist",  bt1);
 	}
 	
 	//임시등록 수주서함
 	@GetMapping("sujubox")
-	public String sujubox() {
-		
-		
+	public String sujubox(Model model) {
+		//수주 박스에 리스트 출력
+		  model.addAttribute("list", ss.suju_select()); 
 		return "sujubox";
 	}
 	
@@ -38,21 +65,21 @@ public class OrderController {
 	@GetMapping("order")
 	public String order(Model model) {
 		//발주 목록 가져오기
-		
 		model.addAttribute("baljulist", bals.balju_select());
 		return "order";
 	}
 	
 	// 발주 페이지
 	@RequestMapping("balju")
-	public String balju() {
+	public String balju(Model model) {
+		//거래처 목록가져오기
+		model.addAttribute("company", bals.company_select());
 		return "balju";
 	}
 	
 	// 발주 등록 insert
 	@RequestMapping("balju_insert")
 	public String balju_insert(BaljuDTO baldto) {
-		System.out.println(baldto);
 		bals.balju_insert(baldto);
 		return "redirect:/order";
 	}
@@ -60,7 +87,6 @@ public class OrderController {
 	// 발주 등록 제품 ajax !
 	@RequestMapping("product_select.json")
 	public ResponseEntity<?> product_select(InventoryDTO id, Model model, HttpSession session) {		
-		System.out.println(bals.product_select());
 		return new ResponseEntity<>(bals.product_select(),HttpStatus.OK);
 	}
 	
