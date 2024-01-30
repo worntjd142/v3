2ceb8dce893d2a57dcb0ecf0d930e53ebd9eb04d
aneob <beloved.eob@gmail.com>
2024-01-30 17:46:29 +0900

    ㅠ

--- src/main/java/we/are/Mapper/OrderMapper.java
+++ src/main/java/we/are/Mapper/OrderMapper.java
@@ -2,28 +2,33 @@ package we.are.Mapper;
 
 import java.util.ArrayList;
 
+import we.are.Model.ConnectionDTO;
 import we.are.Model.CriteriaDTO;
 import we.are.Model.InventoryDTO;
 import we.are.Model.OrderDTO;
 
+
 public interface OrderMapper {
 	
 	// 수주 등록
-	public void order_insert(OrderDTO od);
+	public void order_insert (OrderDTO od);
 	
 	// 수주 목록 리스트
-	public ArrayList<OrderDTO> order_select(CriteriaDTO cd);
+	public ArrayList<OrderDTO> order_select (CriteriaDTO cd);
+	
+	// 거래처명 자동완성
+	public ArrayList<ConnectionDTO> autocomplete (ConnectionDTO cdt);
 	
 	//제품 목록 데이터 가져오기
-	public ArrayList<InventoryDTO> product_select();
+	public ArrayList<InventoryDTO> product_select ();
 	
 	//수주서 발행
-	public int suju_update(int ono);
+	public int suju_update (int ono);
 	
 	// 날짜 검색 : 배열안에 OrderDTO데이터를 담아서 !
-	public ArrayList<OrderDTO> daysearch(CriteriaDTO cd);
+	public ArrayList<OrderDTO> daysearch (CriteriaDTO cd);
 	
-	public OrderDTO sujuletter_select(OrderDTO od);
+	public OrderDTO sujuletter_select (OrderDTO od);
 	
 	// 페이지 전체 건수 total
 	public int total (CriteriaDTO cd);
