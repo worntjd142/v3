638700da149df03a31d7df8e56c60d5a83adbb61
worntjd142 <149766907+worntjd142@users.noreply.github.com>
2024-02-02 17:55:14 +0900

    12w3

--- src/main/java/we/are/Controller/StoreController.java
+++ src/main/java/we/are/Controller/StoreController.java
@@ -3,6 +3,8 @@ package we.are.Controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,17 +30,17 @@ public class StoreController {
 			ArrayList<HashMap<String, Object>> store = ssi.store_select(); // 조인된 결과값을 해시맵에 저장
 			model.addAttribute("stroe", store);// 변경 후 모델에 담아서 전달
 			return "store";
+	
 		}
 		
-		//재고 수량 및 발주 확인
-		/*@GetMapping("pstock")
-		public ResponseEntity<?> pstock(@RequestParam("pstock") String pstock,@RequestParam("ono") int ono,OrderDTO od){
-			od.setOcount(ssi.product_select(pstock));// 재고 수량
-		
-			od.setSdate(ssi.balju_check(ono)); // 발주 확인
-			od.getSdate();
-			return new ResponseEntity<>(od,HttpStatus.OK);
-		}*/
+		@RequestMapping("store_release")
+		public String store_release(Model model) {
+			ArrayList<HashMap<String, Object>> store = ssi.storerelease_select(); // 조인된 결과값을 해시맵에 저장
+			model.addAttribute("stroe", store);// 변경 후 모델에 담아서 전달
+			
+			model.addAttribute("Management" ,ssi.management_select());
+			return "store_release";
+		}
 		
 		//재고 수량 업데이트
 		@GetMapping("pstock_update")
@@ -59,4 +61,32 @@ public class StoreController {
 		public ResponseEntity<?> pcode(@RequestParam("pcode") String pcode){
 			return new ResponseEntity<>(ssi.product_select(pcode),HttpStatus.OK);
 		}
+		
+		/*	//임시등록 수주서1
+		@GetMapping("sujuletter1")
+		public void sujuletter1(OrderDTO od, CompanyDTO cd, HttpSession session, @RequestParam("bno") int bno, Model model) {
+
+			OrderDTO ood = new OrderDTO();//다른 주소의 OrderDTO타입 ood변수 생성		
+			od.setOno(bno);	//parameter로 bno값을 받고 OrderDTO od bno변수에 저장		
+			ood = os.sujuletter_select(od); // od변수에 있는 값을 이용해서 DB의 orders 테이블에서 셀렉하고 결과값을 ood에 저장
+			model.addAttribute("sujuletter", os.sujuletter_select(od)); //셀렉한 값을 model 객체 "sujuletter"변수에 저장
+			cd.setCnumber(ood.getOnumber()); // ood의 저장된 값중 bnumber를 CompanyDTO cd변수 안의 number에 저장
+			model.addAttribute("company", os.sujucom_select(cd.getCnumber())); // 저장받은 number값을 이용해서 DB company에서 결과값을 model 객체 "company"에 저장
+		}
+
+
+			*/
+		//견적서 발행
+		@GetMapping("issuance")
+		public ResponseEntity<?> issuance(OrderDTO od, Model model, HttpSession session,@RequestParam("ono") int ono) {		
+			
+			int result = ssi.osuju_update(ono); //수주상태 업데이트 - > '견적서 발행'
+			System.out.println(result);
+			if(result == 1) {
+				od = ssi.check(ono);
+			}
+			
+			return new ResponseEntity<>(od,HttpStatus.OK);
+		}
+		
 }		
